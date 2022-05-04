efcc03810ea61a9093a4701b73e37edac5f7e1b0
Caroline Patenaude <caroline.patenaude@gmail.com>
2022-05-04 17:48:34 -0400

    Initial commit

---
+++ textsGeyerCatalog.json.js
@@ -0,0 +1,354 @@
+var catData = [
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/CSFLI8VP",
+		"type": "article-journal",
+		"abstract": "Considerable tension among the subfields has existed within the discipline of anthropology. As a result, some anthropology departments have splintered, and the hallmark \"holistic approach\" of anthropology has been considered more myth than reality. However, as promoted by the American Anthropological Association and the American Anthropologist for over one hundred years, enhancing the holistic nature of anthropology remains an important and necessary endeavor. This article provides an introduction to this special issue of the American Anthropologist, which focuses on the subfield of biological anthropology. Hopefully, as a result, increased connections among the subfields will be fostered, for the betterment of both biological anthropology and anthropology in general. The underlying theme of this article and the subtext for the entire special issue is clear: Biological anthropology needs anthropology, and anthropology needs biological anthropology.",
+		"container-title": "American Anthropologist",
+		"DOI": "10.1525/aa.2003.105.1.6",
+		"ISSN": "0002-7294",
+		"issue": "1",
+		"journalAbbreviation": "Am. Anthropol.",
+		"language": "English",
+		"note": "publisher-place: Arlington\npublisher: Amer Anthropological Assoc\nWOS:000181435800001",
+		"page": "6-15",
+		"source": "Web of Science Nextgen",
+		"title": "Keeping biological anthropology in anthropology, and anthropology in biology",
+		"URL": "https://gateway.webofknowledge.com/gateway/Gateway.cgi?GWVersion=2&SrcAuth=DOISource&SrcApp=WOS&KeyAID=10.1525%2Faa.2003.105.1.6&DestApp=DOI&SrcAppSID=USW2EC0E3AN2moj91CIgsE6GRJyV3&SrcJTitle=AMERICAN+ANTHROPOLOGIST&DestDOIRegistrantName=University+of+California+Press",
+		"volume": "105",
+		"author": [
+			{
+				"family": "Calcagno",
+				"given": "J. M."
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					3
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2003",
+					3
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/9BE46D6S",
+		"type": "article-journal",
+		"abstract": "This two-part overview of contemporary Russian anthropology focuses in detail on the work of several scholars and situates it in the changing landscape of Russian academia. The main issue I address is debates about an academic identity of Russian anthropology as 'historical science'. Given that in Western anthropology, history has become one of the leading modes of anthropological analysis and that the turn to history marked a radical repositioning of anthropology's very subject, it is important to explore how such configurations of history and anthropology work in other anthropological traditions and what the reasons are for turning to history or, conversely, avoiding it, for specific national, continental and transnational anthropological schools. In this article, I explore these questions by focusing on anthropology in Russia with an aim of reassembling the relationship between anthropology and history from the point of view of the anthropology of time. I ask what temporal frameworks underscore the relationship between anthropology and history. I explore these understandings ethnographically, that is, through ethnographic interviews with Russian scholars in addition to close readings of their works.",
+		"container-title": "Social Anthropology",
+		"DOI": "10.1111/1469-8676.12628",
+		"ISSN": "0964-0282",
+		"issue": "2",
+		"journalAbbreviation": "Soc. Anthropol.",
+		"language": "English",
+		"note": "publisher-place: Hoboken\npublisher: Wiley\nWOS:000470915000013",
+		"page": "320-335",
+		"source": "Web of Science Nextgen",
+		"title": "Reassembling history and anthropology in Russian anthropology: part I",
+		"title-short": "Reassembling history and anthropology in Russian anthropology",
+		"URL": "https://gateway.webofknowledge.com/gateway/Gateway.cgi?GWVersion=2&SrcAuth=DOISource&SrcApp=WOS&KeyAID=10.1111%2F1469-8676.12628&DestApp=DOI&SrcAppSID=USW2EC0E3AN2moj91CIgsE6GRJyV3&SrcJTitle=SOCIAL+ANTHROPOLOGY&DestDOIRegistrantName=Wiley+%28Blackwell+Publishing%29",
+		"volume": "27",
+		"author": [
+			{
+				"family": "Ssorin-Chaikov",
+				"given": "Nikolai"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					3
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2019",
+					5
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/5FK5JGTB",
+		"type": "article-journal",
+		"abstract": "This paper sets out to briefly explore the definitions of two interrelated subfields of cultural anthropology; psychological anthropology and medical anthropology. This exploration will argue that culture and the individual are intimately intertwined. The theoretical evolution within psychological anthropology will be presented, from the bio-moral classifications of the 'primitive' to modern 'experience near' ethnographies, and fluid understanding of personhood. Theoretical and methodological approaches to mental health will be discussed briefly. Finally, the conclusion will ask the question: what is the future for medical and psychological anthropology?",
+		"container-title": "Irish Journal of Psychological Medicine",
+		"DOI": "10.1017/ipm.2021.34",
+		"ISSN": "0790-9667",
+		"journalAbbreviation": "Irish J. Psychol. Med.",
+		"language": "English",
+		"note": "publisher-place: Cambridge\npublisher: Cambridge Univ Press\nWOS:000780257800001",
+		"page": "PII S0790966721000343",
+		"source": "Web of Science Nextgen",
+		"title": "Psychological anthropology and medical anthropology: a brief history of ideas and concepts",
+		"title-short": "Psychological anthropology and medical anthropology",
+		"URL": "https://gateway.webofknowledge.com/gateway/Gateway.cgi?GWVersion=2&SrcAuth=DOISource&SrcApp=WOS&KeyAID=10.1017%2Fipm.2021.34&DestApp=DOI&SrcAppSID=USW2EC0E3AN2moj91CIgsE6GRJyV3&SrcJTitle=IRISH+JOURNAL+OF+PSYCHOLOGICAL+MEDICINE&DestDOIRegistrantName=Cambridge+University+Press",
+		"author": [
+			{
+				"family": "Sheehan",
+				"given": "Cormac"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					3
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/C755LNXP",
+		"type": "article-journal",
+		"abstract": "Dans nombre de pays, diverses associations religieuses participent souvent à la prestation de soins et de services sociaux, aux côtés de l’État et des organisations non gouvernementales séculières. Cependant, ces groupes religieux ne partagent pas nécessairement les mêmes logiques distributives, notamment en ce qui a trait aux critères de sélection des bénéficiaires. Par exemple, dans la tradition musulmane, les avis sont partagés sur la question de la possibilité pour les non-musulmans de bénéficier de la zakat, l’aumône religieuse obligatoire que les musulmans doivent verser aux plus démunis. Comment ces dons sont-ils encadrés dans les États séculiers modernes et quelles sont les répercussions de cet encadrement sur la légitimation de différentes pratiques philanthropiques ? L’article répond à ces questions en discutant des mesures législatives s’appliquant aux associations caritatives dans le nord de l’Inde et de l’adaptation des pratiques distributives d’organisations philanthropiques musulmanes indiennes. En montrant comment les organisations musulmanes naviguent entre les exigences de l’État et leurs propres principes en matière d’utilisation et de distribution des dons, cet article aborde les rapports de pouvoir entre l’État et le religieux et démontre comment la régulation du religieux façonne de nouvelles formes d’entraide.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088008ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "27-45",
+		"source": "www.erudit.org",
+		"title": "Gouvernance de la philanthropie et pratiques caritatives musulmanes en Inde : les rapports entre religieux et politique vus à travers le don",
+		"title-short": "Gouvernance de la philanthropie et pratiques caritatives musulmanes en Inde",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088008ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Larouche",
+				"given": "Catherine"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/SC5UECJA",
+		"type": "article-journal",
+		"abstract": "Les pratiques humanitaires musulmanes contemporaines sont imprégnées du langage de la solidarité religieuse. Pourtant, les historiens et les politologues ont montré que les déclarations d’unité islamique ne sont pas défendables et que le discours au sujet de la communauté religieuse (l’oumma) a une fonction idéologique. Plus largement, les tentatives des sciences sociales de comprendre l’oumma se fondent souvent sur sa répudiation théologique des catégories politiques modernes (nation-État-territoire). Au contraire, à partir d’un travail de terrain mené auprès d’organismes d’aide humanitaire en Jordanie et en tant que contribution à l’anthropologie politique de la religion, cet article plaide en faveur d’une autre façon de comprendre l’oumma : en n’exposant pas la différence entre l’idéal et la réalité, mais en envisageant plutôt la communauté religieuse sous la forme de sa perte.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088009ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "47-66",
+		"source": "www.erudit.org",
+		"title": "Les croyants comme un corps souffrant : remarques depuis la frontière jordano-syrienne",
+		"title-short": "Les croyants comme un corps souffrant",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088009ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Iqbal",
+				"given": "Basit"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/GQVFVRJ9",
+		"type": "article-journal",
+		"abstract": "Le djihad contemporain est souvent perçu soit comme la négation pure du concept de « souveraineté de l’État », soit comme la recherche d’une version islamique de la souveraineté de l’État par la mise en oeuvre de la charia. Au contraire, cet article montre comment les djihads transnationaux en particulier mettent l’accent sur une logique de solidarité entre les communautés islamiques sans nécessairement rejeter en bloc l’autorité de l’État-nation. Il met au jour cette logique de solidarité sur le plan théorique au moyen d’une analyse de la théologie politique du théoricien du djihad palestinien ‘Abd Allah ‘Azzam, tant dans ses écrits juridiques que dans ceux portant sur les miracles dans le djihad en Afghanistan. C’est la solidarité, plutôt que la souveraineté, qui éclaire les difficultés et les dilemmes quotidiens des participants au djihad, comme le démontrent les débats entre les combattants du djihad arabes lors de la guerre de 1992-1995 en Bosnie-Herzégovine au sujet des règles islamiques régissant les mariages avec des femmes bosniaques.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088010ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "67-85",
+		"source": "www.erudit.org",
+		"title": "La solidarité contre la souveraineté : penser autrement le phénomène du « djihad sans frontières »",
+		"title-short": "La solidarité contre la souveraineté",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088010ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Li",
+				"given": "Darryl"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/8N52TPD9",
+		"type": "article-journal",
+		"abstract": "Cet article brosse un portrait comparatif de la mobilisation de la dimension spirituelle par les nations autochtones du Canada et aborigènes de l’Australie en contexte juridique. Plus précisément, il s’agit de vérifier, à la lumière d’une série de jugements rendus par les tribunaux, dans quelle mesure les particularismes du régime normatif de chaque pays influencent : 1) la manière dont cette dimension spirituelle est représentée d’un point de vue ethnographique par les intervenants autochtones ; 2) la qualification juridique permettant de l’introduire et de la justifier ; et 3) l’appréciation que lui réservent les magistrats. À travers les similitudes et les divergences qu’elle révèle, cette comparaison montre la nécessité pour les Autochtones et les Aborigènes de moduler, voire de dénaturer la représentation de leurs particularismes spirituels pour les rendre compatibles avec les canons idéologiques et normatifs qui guident les tribunaux et, par conséquent, les limites des cadres normatifs actuels sur le plan de la réception et de l’appréciation de ces spécificités culturelles autochtones.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088011ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "87-105",
+		"source": "www.erudit.org",
+		"title": "Les tribunaux et la configuration des identités spirituelles autochtones : une comparaison des contextes canadien et australien",
+		"title-short": "Les tribunaux et la configuration des identités spirituelles autochtones",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088011ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Gélinas",
+				"given": "Claude"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/4TQVYHB8",
+		"type": "article-journal",
+		"abstract": "Dans cet article, j’examine la façon dont se recoupent l’éthique et l’institutionnel au Centre de formation biblique et ministérielle d’Anagkazo (Anagkazo Bible and Ministry Training Center), une école biblique évangéliste (pentecôtiste) charismatique fondée par Lighthouse Chapel International (LCI), de confession ghanéenne. Je reconstruis de façon ethnographique le processus d’apprentissage et d’incorporation de la vertu d’obéissance dans cette école, et montre comment la volonté souveraine de Dieu est défléchie par un appareil disciplinaire aux diverses inflexions organisationnelles, dont certaines relèvent très fortement du management. Les vertus chrétiennes universelles que recouvre l’obéissance, telles que l’« humilité », l’« excellence » et la « fidélité », sont cultivées par les étudiants dans le cadre d’un appareil disciplinaire qui confère au corps pastoral de LCI une identité d’entreprise distincte. Ces processus qui, simultanément, intensifient et normalisent la piété évangélique exigent que nous comprenions mieux la façon dont la formation du sujet éthique et ses allégeances institutionnelles peuvent être juxtaposées, tout en conservant un caractère ontologique distinctif pour les praticiens. Je mets en exergue certaines contributions que ce débat peut apporter à l’anthropologie de l’éthique et du christianisme.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088012ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "107-129",
+		"source": "www.erudit.org",
+		"title": "Les vertus de l’obéissance : éthique et gouvernement dans une école biblique pentecôtiste",
+		"title-short": "Les vertus de l’obéissance",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088012ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Reinhardt",
+				"given": "Bruno"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	},
+	{
+		"id": "http://zotero.org/users/local/tzwIbLTr/items/HZ384T2C",
+		"type": "article-journal",
+		"abstract": "Entre 2003 et 2012, des résidents du village de Jeita (Liban) se sont livrés à des expériences mystiques aux côtés d’une femme sujette à des visions de la Vierge Marie. Ils ont prié ensemble, interprété l’enseignement de la Vierge, construit un sanctuaire, fait paraître des livres et inventé un langage symbolique qui transcende les divisions en action au sein de la société libanaise — et ce, jusqu’en 2012, date à laquelle la police nationale mit fin à leurs activités sur ordre du Conseil d’État. Cette note de recherche rend compte d’un premier terrain de recherche mené auprès d’anciens membres du groupe mystique et des autorités ecclésiales qui se sont inquiétées de voir une « hérésie » prendre forme à Jeita. L’analyse proposée suggère que l’État moderne fait bien plus que gouverner le religieux : il gouverne également par le religieux.",
+		"container-title": "Anthropologie et Sociétés",
+		"DOI": "10.7202/1088013ar",
+		"ISSN": "0702-8997, 1703-7921",
+		"issue": "3",
+		"journalAbbreviation": "as",
+		"language": "fr",
+		"note": "publisher: Département d’anthropologie de l’Université Laval",
+		"page": "131-149",
+		"source": "www.erudit.org",
+		"title": "Marsillah de Jeita : hérésie, sexualité et raison d’État (note de recherche)",
+		"title-short": "Marsillah de Jeita",
+		"URL": "https://www.erudit.org/fr/revues/as/2021-v45-n3-as06901/1088013ar/",
+		"volume": "45",
+		"author": [
+			{
+				"family": "Landry",
+				"given": "Jean-Michel"
+			}
+		],
+		"accessed": {
+			"date-parts": [
+				[
+					"2022",
+					5,
+					4
+				]
+			]
+		},
+		"issued": {
+			"date-parts": [
+				[
+					"2021"
+				]
+			]
+		}
+	}
+]
---
+++ textsInterface.json.js
@@ -0,0 +1,165 @@
+var texts =
+    {
+        "headline":
+            {
+                "english": "Test de catalogue Zotero",
+                "german": "Test de catalogue Zotero"
+            }
+        ,
+        "stiftFlorianTip":
+            {
+                "english": "Go to Stift Florian's Website",
+                "german": "Gehe zur Website von Stift Florian"
+            }
+        ,
+        "digitalOrientalistTip":
+            {
+                "english": "Made by L.W.C. van Lit",
+                "german": "hergestellt von Dr. L.W.C. van Lit"
+            }
+        ,
+        "flagTip":
+            {
+                "english": "Switch to English",
+                "german": "Auf deutsch umstellen"
+            }
+        ,
+        "welcomeHeader":
+            {
+                "english": "Bonjour",
+                "german": "Bonjour"
+            }
+        ,
+        "welcomeBody":
+            {
+                "english": "Allo!",
+                "german": "Allo!"
+            }
+        ,
+        "searchButton":
+            {
+                "english": "Chercher",
+                "german": "Chercher"
+            }
+        ,
+        "searchBox":
+            {
+                "english": "Rechercher...",
+                "german": "Rechercher..."
+            }
+        ,
+        "searchTip":
+            {
+                "english": "Inscrire un terme et cliquer Chercher",
+                "german": "Inscrire un terme et cliquer Chercher"
+            }
+        ,
+        "sortBy":
+            {
+                "english": "Trier par: ",
+                "german": "Trier par: "
+            }
+        ,
+        "sortTitleCaption":
+            {
+                "english": "Titre",
+                "german": "Titre"
+            }
+        ,
+        "sortAuthorCaption":
+            {
+                "english": "Auteur",
+                "german": "Auteur"
+            }
+        ,
+        "sortYearCaption":
+            {
+                "english": "Année",
+                "german": "Année"
+            }
+        ,
+        "beginTitleCatalog":
+            {
+                "english": "Catalogue",
+                "german": "Catalogue"
+            }
+        ,
+        "noResultsTitleCatalog":
+            {
+                "english": "No results, showing all entries instead:",
+                "german": "Keine Ergebnisse, stattdessen werden alle EintrÃ¤ge angezeigt:"
+            }
+        ,
+        "againTitleCatalog":
+            {
+                "english": "Showing all entries again:",
+                "german": "Alle EintrÃ¤ge erneut angezeigt:"
+            }
+        ,
+        "foundItemsCatalog":
+            {
+                "english": "Nombre de résultats:",
+                "german": " Nombre de résultats:"
+            },
+        "authorCatalog":
+            {
+                "english": "Auteur: ",
+                "german": "Auteur: "
+            }
+        ,
+        "editorCatalog":
+            {
+                "english": "Éditeur: ",
+                "german": "Éditeur: "
+            },
+        "translatorCatalog":
+            {
+                "english": "Traducteur: ",
+                "german": "Traducteur: "
+            },
+        "noAuthorCatalog":
+            {
+                "english": "No author details. ",
+                "german": "Keine Autorendetails. "
+            },
+        "numberVolumesCatalog":
+            {
+                "english": "Number of volumes: ",
+                "german": "Anzahl Banden: "
+            },
+        "publisherCatalog":
+            {
+                "english": "Publisher: ",
+                "german": "Verlag: "
+            },
+        "placeCatalog":
+            {
+                "english": "Lieu: ",
+                "german": "Lieu: "
+            },
+        "yearCatalog":
+            {
+                "english": "Annee: ",
+                "german": "Annee: "
+            },
+        "additionalCatalog":
+            {
+                "english": "Résumé: ",
+                "german": "Résumé: "
+            },
+        "callNumberCatalog":
+            {
+                "english": "Cote: ",
+                "german": "Cote: "
+            },
+        "urlCatalog":
+            {
+                "english": "pdf",
+                "german": "pdf"
+            },
+        "closeCatalog":
+            {
+                "english": "Fermer",
+                "german": "Fermer"
+            }
+    }
